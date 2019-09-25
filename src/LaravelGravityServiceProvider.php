7136a8b588b9df8b5864f5cd17b046bbc782f4ae
Adam Glaysher <adamglaysher@gmail.com>
2019-09-25 16:43:05 +0100

    init

---
+++ src/LaravelGravityServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bubalubs\LaravelGravity;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelGravityServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
