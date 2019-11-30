3cfdd1026f373a4a2ce0050744188ca5480e884d
Adam Glaysher <adamglaysher@gmail.com>
2019-11-30 05:27:50 +0000

    Tooltips on texteditor

--- src/resources/js/text-editor.vue
+++ src/resources/js/text-editor.vue
@@ -13,6 +13,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.bold() }"
                     @click.prevent="commands.bold"
+                    v-tooltip="'Bold'"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18.287 11.121c1.588-1.121 2.713-3.018 2.713-5.093 0-2.946-1.918-5.951-7.093-6.028h-11.907v2.042c1.996 0 3 .751 3 2.683v14.667c0 1.689-.558 2.608-3 2.608v2h11.123c6.334 0 8.877-3.599 8.877-7.038 0-2.538-1.417-4.67-3.713-5.841zm-8.287-8.121h2.094c2.357 0 4.126 1.063 4.126 3.375 0 2.035-1.452 3.625-3.513 3.625h-2.707v-7zm2.701 18h-2.701v-8h2.781c2.26.024 3.927 1.636 3.927 3.667 0 2.008-1.226 4.333-4.007 4.333z"/></svg>
                 </button>
@@ -21,6 +22,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.italic() }"
                     @click.prevent="commands.italic"
+                    v-tooltip="'Italic'"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.565 20.827c-.361.732-.068 1.173.655 1.173h1.78v2h-9v-2h.897c1.356 0 1.673-.916 2.157-1.773l8.349-16.89c.403-.852-.149-1.337-.855-1.337h-1.548v-2h9v2h-.84c-1.169 0-1.596.646-2.06 1.516l-8.535 17.311z"/></svg>
                 </button>
@@ -29,6 +31,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.underline() }"
                     @click.prevent="commands.underline"
+                    v-tooltip="'Underline'"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 24h-16v-2h16v2zm-5-24v1.973c1.619 0 2 .926 2 1.497v9.056c0 2.822-2.161 4.507-5 4.507s-5-1.685-5-4.507v-9.056c0-.571.381-1.497 2-1.497v-1.973h-7v1.973c1.66 0 2 .575 2 1.497v8.828c0 5.175 3.096 7.702 8 7.702 4.899 0 8-2.527 8-7.702v-8.828c0-.922.34-1.497 2-1.497v-1.973h-7z"/></svg>
                 </button>
@@ -37,6 +40,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.heading({ level: 1 }) }"
                     @click.prevent="commands.heading({ level: 1 })"
+                    v-tooltip="'Header 1'"
                 >
                     H1
                 </button>
@@ -45,6 +49,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.heading({ level: 2 }) }"
                     @click.prevent="commands.heading({ level: 2 })"
+                    v-tooltip="'Header 2'"
                 >
                     H2
                 </button>
@@ -53,6 +58,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.heading({ level: 3 }) }"
                     @click.prevent="commands.heading({ level: 3 })"
+                    v-tooltip="'Header 3'"
                 >
                     H3
                 </button>
@@ -61,6 +67,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.heading({ level: 4 }) }"
                     @click.prevent="commands.heading({ level: 4 })"
+                    v-tooltip="'Header 4'"
                 >
                     H4
                 </button>
@@ -69,6 +76,7 @@
                     class="button"
                     :class="{ 'is-active': isActive.bullet_list() }"
                     @click.prevent="commands.bullet_list"
+                    v-tooltip="'List'"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 1h17v2h-17v-2zm0 7h17v-2h-17v2zm0 5h17v-2h-17v2zm0 5h17v-2h-17v2zm0 5h17v-2h-17v2zm-5-22c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm0 9c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm0 9c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2z"/></svg>
                 </button>
@@ -76,6 +84,7 @@
                 <button
                     class="button"
                     @click.prevent="commands.horizontal_rule"
+                    v-tooltip="'Horizontal Seperator'"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8 14h-8v-2h8.672l-.672 2zm-8 10v-8h24v8h-24zm2-2h20v-4h-2v2h-1v-2h-2v3h-1v-3h-2v2h-1v-2h-2v2h-1v-2h-2v3h-1v-3h-2v2h-1v-2h-2v4zm9.398-12.26l-1.398 4.26 4.227-1.432-2.829-2.828zm9.774-9.74l2.828 2.828-8.587 8.554-2.828-2.828 8.587-8.554z"/></svg>
                 </button>
@@ -83,6 +92,7 @@
                 <button
                     class="button"
                     @click.prevent="commands.undo"
+                    v-tooltip="'Undo'"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18.885 3.515c-4.617-4.618-12.056-4.676-16.756-.195l-2.129-2.258v7.938h7.484l-2.066-2.191c2.82-2.706 7.297-2.676 10.073.1 4.341 4.341 1.737 12.291-5.491 12.291v4.8c3.708 0 6.614-1.244 8.885-3.515 4.686-4.686 4.686-12.284 0-16.97z"/></svg>
                 </button>
@@ -90,22 +100,10 @@
                 <button
                     class="button"
                     @click.prevent="commands.redo"
+                    v-tooltip="'Redo'"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5.115 3.515c4.617-4.618 12.056-4.676 16.756-.195l2.129-2.258v7.938h-7.484l2.066-2.191c-2.82-2.706-7.297-2.676-10.073.1-4.341 4.341-1.737 12.291 5.491 12.291v4.8c-3.708 0-6.614-1.244-8.885-3.515-4.686-4.686-4.686-12.284 0-16.97z"/></svg>
                 </button>
-
-                <div v-if="showInfoHelperButtons">
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('WIFI')">WIFI</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('TV/DVD/CD player')">TV/DVD/CD player</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Dishwasher')">Dishwasher</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Washing machine')">Washing machine</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Tumble dryer')">Tumble dryer</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Bedding and towels included')">Bedding and towels</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Garden with BBQ and patio furniture')">BBQ</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Cleaning materials included')">Cleaning materials</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Complimentary Welcome Hamper')">Welcome Hamper</button>
-                    <button class="button" style="width:auto;margin-top:3px" @click.prevent="addListItem('Dogs welcome but subject to approval')">Dogs Welcome</button>
-                </div>
             </div>
         </editor-menu-bar>
 
@@ -159,8 +157,7 @@
         props: [
             'name',
             'placeholder',
-            'value',
-            'showInfoHelperButtons'
+            'value'
         ],
         
         components: {
