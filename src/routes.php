0a4721aa064f8d4ebdefbc8a9f8e97650fd912ee
Adam Glaysher <adamglaysher@gmail.com>
2019-12-30 08:22:24 +0000

    Small route fix

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,27 @@ Route::middleware('web')->group(function () {
     {
         Route::get('/', 'DashboardController@view');
 
+        Route::middleware('can:use_tools_in_admin')->group(function () {
+            Route::get('pages', 'PagesController@manage');
+            Route::get('pages/{page}/fields', 'PageFieldsController@edit');
+            Route::get('global/fields/manage', 'GlobalContentFieldsController@manage');
+            Route::get('entities', 'EntitiesController@manage');
+            Route::get('entities/{entity}/fields', 'EntityFieldsController@edit');
+
+            Route::post('pages/create', 'PagesController@create');
+            Route::post('entities/create', 'EntitiesController@create');
+            Route::post('pages/{page}/fields/create', 'PageFieldsController@create');
+            Route::post('entities/{page}/fields/create', 'EntityFieldsController@create');
+            Route::post('global/fields/create', 'GlobalContentFieldsController@create');
+            Route::post('entities/{entity}/fields', 'EntityFieldsController@create');
+
+            Route::delete('pages/{id}/delete', 'PagesController@delete');
+            Route::delete('entities/{id}/delete', 'EntitiesController@delete');
+            Route::delete('pages/{page}/fields/{fieldID}/delete', 'PageFieldsController@delete');
+            Route::delete('entities/{page}/fields/{fieldID}/delete', 'EntityFieldsController@delete');
+            Route::delete('global/fields/{fieldID}/delete', 'GlobalContentFieldsController@delete');
+        });
+
         Route::middleware('can:edit_entities_in_admin')->group(function () {
             Route::get('entities/{entity}', 'EntityController@list');
             Route::get('entities/{entity}/create', 'EntityController@viewCreateForm');
@@ -36,27 +57,6 @@ Route::middleware('web')->group(function () {
             Route::get('users/{id}', 'UsersController@edit');
 
             Route::post('users/{id}/update', 'UsersController@update');
-        });
-
-        Route::middleware('can:use_tools_in_admin')->group(function () {
-            Route::get('pages', 'PagesController@manage');
-            Route::get('pages/{page}/fields', 'PageFieldsController@edit');
-            Route::get('global/fields/manage', 'GlobalContentFieldsController@manage');
-            Route::get('entities', 'EntitiesController@manage');
-            Route::get('entities/{entity}/fields', 'EntityFieldsController@edit');
-
-            Route::post('pages/create', 'PagesController@create');
-            Route::post('entities/create', 'EntitiesController@create');
-            Route::post('pages/{page}/fields/create', 'PageFieldsController@create');
-            Route::post('entities/{page}/fields/create', 'EntityFieldsController@create');
-            Route::post('global/fields/create', 'GlobalContentFieldsController@create');
-            Route::post('entities/{entity}/fields', 'EntityFieldsController@create');
-
-            Route::delete('pages/{id}/delete', 'PagesController@delete');
-            Route::delete('entities/{id}/delete', 'EntitiesController@delete');
-            Route::delete('pages/{page}/fields/{fieldID}/delete', 'PageFieldsController@delete');
-            Route::delete('entities/{page}/fields/{fieldID}/delete', 'EntityFieldsController@delete');
-            Route::delete('global/fields/{fieldID}/delete', 'GlobalContentFieldsController@delete');
         });
     });
 
